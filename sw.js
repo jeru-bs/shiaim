8526dc350530f92e1e729dc2cee76d2e2943777b
jeru-bs <azaidman1@gmail.com>
2026-06-30 22:35:15 +0300

    Update CACHE_NAME to shiaim-v38

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // v31
-const CACHE_NAME = 'shiaim-v37';
+const CACHE_NAME = 'shiaim-v38';
 const GOOD_HTML = 'https://raw.githubusercontent.com/jeru-bs/shiaim/fc784d211c2804714f80be959b4d8527c891087d/index.html';
 
 self.addEventListener('install', e => {
